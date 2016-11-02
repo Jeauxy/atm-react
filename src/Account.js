f93a7e17a7930086a999fcf4f9f5f5e57f54abe4
Joseph Callaway <jeauxcal@gmail.com>
2016-11-01 21:13:42 -0500

    Forgot to commit. Finished with #1-4

--- src/Account.js
+++ src/Account.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
 
 export default class Account extends Component {
+  constructor(){
+    super();
+    this.state={
+      balance: 0
+    }
+    this._deposit = this._deposit.bind(this);
+    this._withdraw = this._withdraw.bind(this);
+  }
+  _deposit(){
+    // get user input and add it to the balance
+    let depositAmount = this.refs.newAmount.value
+    // reset the balance state
+    this.setState({
+      balance: this.state.balance + +depositAmount
+    })
+  }
+  _withdraw(){
+    // get user input and deduct it from the balance
+    let withdrawAmount = this.refs.newAmount.value
+    if (this.state.balance > withdrawAmount){
+      this.setState({
+        balance: this.state.balance + -withdrawAmount
+      })}else{
+        alert("Insufficient Funds")
+      }
+    // reset the balance state
+  }
+  _handleClick(){
+    this.props.withdraw();
+  }
+  render(){
+    return(
+      <i onClick={this._handleClick.bind(this)} className="zero"></i>
+    )
+  }
+
+
   render() {
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className="balance" >${this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" ref="newAmount"/>
+        <input type="button" value="Deposit" onClick={this._deposit}/>
+        <input type="button" value="Withdraw" onClick={this._withdraw}/>
       </div>
     )
   }
